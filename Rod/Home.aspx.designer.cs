2540232c93a18d7ba91dc42a66733f5a7d0c147b
abdulaziz alsunaydi <azozb997@gmail.com>
2022-06-14 22:46:12 +0300

    with the cs files

--- Rod/Home.aspx.designer.cs
+++ Rod/Home.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Rod
     {
 
         /// <summary>
+        /// HideIfUserLoggedIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl HideIfUserLoggedIn;
+
+        /// <summary>
         /// homeImage control.
         /// </summary>
         /// <remarks>
@@ -40,5 +49,41 @@ namespace Rod
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink questionsLink;
+
+        /// <summary>
+        /// homeLoggedIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl homeLoggedIn;
+
+        /// <summary>
+        /// selected control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink selected;
+
+        /// <summary>
+        /// TagsLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink TagsLink;
+
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
