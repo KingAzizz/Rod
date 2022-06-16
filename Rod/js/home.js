1fb33f1775344ff60531fd461ebe7a48474941be
abdulaziz alsunaydi <azozb997@gmail.com>
2022-06-16 21:12:55 +0300

    added new file in js for home page to work on function that open side navbar and close it in mobile screen

---
+++ Rod/js/home.js
@@ -0,0 +1,13 @@
+﻿let triggerSideNav = document.getElementById("triggerSideNav")
+let sidenav = document.getElementById("ContentPlaceHolder1_sidenav")
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(sidenav)
+    triggerSideNav.addEventListener("click", () => {
+       
+        sidenav.classList.toggle("display")
+        
+
+    })
+});
