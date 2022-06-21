17118f7a4a4b478f143eb8ba4ce0898960bd696c
abdulaziz alsunaydi <azozb997@gmail.com>
2022-06-21 20:37:50 +0300

    added method that takes date and turn it to (2 min ago or month ago) added the database code that get the questions and show it in the home page with all of the details including username question title votes answers

--- Rod/Home.aspx.designer.cs
+++ Rod/Home.aspx.designer.cs
@@ -130,5 +130,14 @@ namespace Rod
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlButton askQuestion;
+
+        /// <summary>
+        /// sectionQuestions control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl sectionQuestions;
     }
 }
