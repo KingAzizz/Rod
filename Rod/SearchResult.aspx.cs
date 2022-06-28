3c6ca26afc74a052a37907f56ab6c450c08bece2
abdulaziz alsunaydi <azozb997@gmail.com>
2022-06-28 23:08:27 +0300

    added filter methods by newest and highest ratings, and get the data from database query

--- Rod/SearchResult.aspx.cs
+++ Rod/SearchResult.aspx.cs
@@ -4,14 +4,207 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
 namespace Rod
 {
     public partial class SearchResult : System.Web.UI.Page
     {
+        public static string RelativeDate(DateTime theDate)
+        {
+            const int SECOND = 1;
+            const int MINUTE = 60 * SECOND;
+            const int HOUR = 60 * MINUTE;
+            const int DAY = 24 * HOUR;
+            const int MONTH = 30 * DAY;
+
+            var ts = new TimeSpan(DateTime.UtcNow.Ticks - theDate.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
+
+            if (delta < 1 * MINUTE)
+                return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
+
+            if (delta < 2 * MINUTE)
+                return "a minute ago";
+
+            if (delta < 45 * MINUTE)
+                return ts.Minutes + " minutes ago";
+
+            if (delta < 90 * MINUTE)
+                return "an hour ago";
+
+            if (delta < 24 * HOUR)
+                return ts.Hours + " hours ago";
+
+            if (delta < 48 * HOUR)
+                return "yesterday";
+
+            if (delta < 30 * DAY)
+                return ts.Days + " days ago";
+
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "one month ago" : months + " months ago";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? "one year ago" : years + " years ago";
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                searchedItemText.InnerText = " النتائج " +  "[" + Request.QueryString["searched"].ToString() +"]";
+                string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aziz\source\repos\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
 
+                SqlConnection con = new SqlConnection(cs);
+
+                con.Open();
+
+                string SearchResult = @"SELECT COUNT(*) OVER(),[User].username,[User].reputation,[Post].title,[Post].creationDate,[Post].upvoteCount,[Post].answerCount
+                    FROM [User]
+                    INNER JOIN [Post]
+                    ON [User].id = [Post].userId
+                    where [Post].title LIKE N'%" + Request.QueryString["searched"].ToString() + "%'";
+               
+                SqlCommand cmd = new SqlCommand(SearchResult, con);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                bool count = false;
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        if(count == false)
+                        {
+                            resultCount.InnerText = "["+dr.GetValue(0).ToString()+"]" + " نتيجة";
+                        }
+                        count = true;
+                       
+                       searchResult.InnerHtml +=  " " +
+                       " <div class='question'>" +
+                         "<div class='votesAnswers'>" +
+
+                          "<h3><span>" + dr.GetValue(5).ToString() + "</span> التقييم</h3>" +
+                        "<div class='answersContainer'>" +
+                        "<h3><span>" + dr.GetValue(6).ToString() + "</span> الأجابات</h3>" +
+                         "</div>  </div>" +
+
+
+                       " <div class='questionTitle'>" +
+                           " <p>" + dr.GetValue(3).ToString() + "</p> </div>" +
+
+                       " <div class='usernameQuestionDetails'>" +
+                        "<h2><span>" + dr.GetValue(1).ToString() + "</span>   <span>" + dr.GetValue(2).ToString() + "</span></h2>" +
+                       " <p>" + RelativeDate(Convert.ToDateTime(dr.GetValue(4))) + "</p></div></div>";
+                    }
+                }
+                con.Close();
+            }
+        }
+
+        protected void HighRatingFilter(object sender, EventArgs e)
+        {
+            searchResult.InnerHtml = "";
+            searchedItemText.InnerText = " النتائج " + "[" + Request.QueryString["searched"].ToString() + "]";
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aziz\source\repos\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(cs);
+
+            con.Open();
+            string SearchResultByNewest = @"SELECT COUNT(*) OVER(),[User].username,[User].reputation,[Post].title,[Post].creationDate,[Post].upvoteCount,[Post].answerCount
+                    FROM [User]
+                    INNER JOIN [Post]
+                    ON [User].id = [Post].userId
+                    where [Post].title LIKE N'%" + Request.QueryString["searched"].ToString() + "%' order by [Post].upvoteCount DESC;";
+
+            SqlCommand cmd = new SqlCommand(SearchResultByNewest, con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            bool count = false;
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    if (count == false)
+                    {
+                        resultCount.InnerText = "[" + dr.GetValue(0).ToString() + "]" + " نتيجة";
+                    }
+                    count = true;
+
+                    searchResult.InnerHtml += " " +
+                    " <div class='question'>" +
+                      "<div class='votesAnswers'>" +
+
+                       "<h3><span>" + dr.GetValue(5).ToString() + "</span> التقييم</h3>" +
+                     "<div class='answersContainer'>" +
+                     "<h3><span>" + dr.GetValue(6).ToString() + "</span> الأجابات</h3>" +
+                      "</div>  </div>" +
+
+
+                    " <div class='questionTitle'>" +
+                        " <p>" + dr.GetValue(3).ToString() + "</p> </div>" +
+
+                    " <div class='usernameQuestionDetails'>" +
+                     "<h2><span>" + dr.GetValue(1).ToString() + "</span>   <span>" + dr.GetValue(2).ToString() + "</span></h2>" +
+                    " <p>" + RelativeDate(Convert.ToDateTime(dr.GetValue(4))) + "</p></div></div>";
+                }
+            }
+            con.Close();
+        }
+        protected void NewestFilter(object sender, EventArgs e)
+        {
+            searchResult.InnerHtml = "";
+            searchedItemText.InnerText = " النتائج " + "[" + Request.QueryString["searched"].ToString() + "]";
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aziz\source\repos\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(cs);
+
+            con.Open();
+            string SearchResultByNewest = @"SELECT COUNT(*) OVER(),[User].username,[User].reputation,[Post].title,[Post].creationDate,[Post].upvoteCount,[Post].answerCount
+                    FROM [User]
+                    INNER JOIN [Post]
+                    ON [User].id = [Post].userId
+                    where [Post].title LIKE N'%" + Request.QueryString["searched"].ToString() + "%' order by [Post].creationDate DESC;";
+
+            SqlCommand cmd = new SqlCommand(SearchResultByNewest, con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            bool count = false;
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    if (count == false)
+                    {
+                        resultCount.InnerText = "[" + dr.GetValue(0).ToString() + "]" + " نتيجة";
+                    }
+                    count = true;
+
+                    searchResult.InnerHtml += " " +
+                    " <div class='question'>" +
+                      "<div class='votesAnswers'>" +
+
+                       "<h3><span>" + dr.GetValue(5).ToString() + "</span> التقييم</h3>" +
+                     "<div class='answersContainer'>" +
+                     "<h3><span>" + dr.GetValue(6).ToString() + "</span> الأجابات</h3>" +
+                      "</div>  </div>" +
+
+
+                    " <div class='questionTitle'>" +
+                        " <p>" + dr.GetValue(3).ToString() + "</p> </div>" +
+
+                    " <div class='usernameQuestionDetails'>" +
+                     "<h2><span>" + dr.GetValue(1).ToString() + "</span>   <span>" + dr.GetValue(2).ToString() + "</span></h2>" +
+                    " <p>" + RelativeDate(Convert.ToDateTime(dr.GetValue(4))) + "</p></div></div>";
+                }
+            }
+            con.Close();
         }
     }
 }
