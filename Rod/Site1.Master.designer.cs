9b7f9851dd7eeaa5def9feffa22346bf1d6d65ce
abdulaziz alsunaydi <azozb997@gmail.com>
2022-06-28 23:08:54 +0300

    added the button to trigger the search input

--- Rod/Site1.Master.designer.cs
+++ Rod/Site1.Master.designer.cs
@@ -51,6 +51,15 @@ namespace Rod
         protected global::System.Web.UI.WebControls.TextBox searchText;
 
         /// <summary>
+        /// searchButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button searchButton;
+
+        /// <summary>
         /// loginLink control.
         /// </summary>
         /// <remarks>
