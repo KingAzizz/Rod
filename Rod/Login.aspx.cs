f62147a5fbe050c52de2444ffee7b8db96e6dcd4
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-05 22:23:19 +0300

    login page backend

--- Rod/Login.aspx.cs
+++ Rod/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Rod
 {
@@ -11,6 +12,37 @@ namespace Rod
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(Session["loggedIn"] != null)
+            {
+                Response.Redirect("~/Home.aspx");
+            }
+        }
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            string username = usernameTxt.Text;
+            string password = passwordTxt.Text;
+
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aziz\source\repos\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            string loginQuery = @"select * from [User] where [username] = '" + username + "' and CONVERT(VARCHAR,[password]) = '" + password + "'";
+
+            SqlCommand cmd = new SqlCommand(loginQuery, con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                Session["username"] = username;
+                Session["loggedIn"] = "true";
+                Response.Redirect("~/Home.aspx");
+            }
+            else
+            {
+                invalidCredentials.Text = "اسم المستخدم او كلمة المرور غير صحيحة";
+            }
+
+
 
         }
     }
