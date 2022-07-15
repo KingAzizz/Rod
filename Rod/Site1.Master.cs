3aa4e5a4839a0a229884c9c1b3ec354871aaadd1
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-15 22:51:28 +0300

    added and fixed bugs that shows when user is logged in

--- Rod/Site1.Master.cs
+++ Rod/Site1.Master.cs
@@ -23,15 +23,16 @@ namespace Rod
                 inboxLink.NavigateUrl = "Registration.aspx";
                 inboxLink.Style.Add("color", "#4F6BFF");
 
-                profileImageLink.Text = "دخول";
-                profileImageLink.NavigateUrl = "Login.aspx";
-                profileImageLink.Style.Add("color", "#4F6BFF");
-
+                loginLink.Visible = true;
+                profile.Visible = false;
+                loginHyLink.Style.Add("color", "#4F6BFF");
+                
 
             }
             else {
-
-                string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aziz\source\repos\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
+                    loginLink.Visible = false;
+                    profile.Visible = true;
+                    string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aziz\source\repos\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
                 SqlConnection con = new SqlConnection(cs);
                 con.Open();
 
@@ -53,6 +54,11 @@ namespace Rod
         protected void SearchTrigger(object sender, EventArgs e)
         {
             Response.Redirect("SearchResult.aspx?searched=" + searchText.Text);
+        }
+        protected void Logout(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("~/Login.aspx");
         }
     }
 }
