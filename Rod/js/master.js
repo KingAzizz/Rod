e53ce6942cc3e4343f99e35fa159c59896fa760e
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-15 22:44:56 +0300

    created new js file to handle when user clicks in his image to show dropdownlist , handle when user clicks on search icon on mobile screen and show the user search textbox

---
+++ Rod/js/master.js
@@ -0,0 +1,17 @@
+﻿let dropdownContent = document.querySelector(".dropdownContent");
+let profileMenu = document.getElementById("profileMenu");
+let searchTextBox = document.getElementById("searchText")
+searchInput.addEventListener("click", (event) => {
+    event.stopPropagation();
+    
+    searchTextBox.classList.toggle("showTextBox")
+})
+profileMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+    dropdownContent.classList.toggle("drop")
+    
+});
+document.addEventListener("click", function () {
+    searchTextBox.classList.remove("showTextBox");
+    dropdownContent.classList.remove("drop")
+});
