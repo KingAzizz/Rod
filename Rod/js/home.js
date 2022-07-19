259cbea21625d48d6afcdfff003ce35d5b225362
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-19 21:09:21 +0300

    added eventlistener that will hide the side navbar if user clicks anywhere in the page

--- Rod/js/home.js
+++ Rod/js/home.js
@@ -4,10 +4,15 @@ let sidenav = document.getElementById("ContentPlaceHolder1_sidenav")
 
 document.addEventListener('DOMContentLoaded', function () {
     
-    triggerSideNav.addEventListener("click", () => {
-       
+    triggerSideNav.addEventListener("click", (event) => {
+        event.stopPropagation();
         sidenav.classList.toggle("display")
         
 
     })
+});
+
+document.addEventListener("click", function () {
+    sidenav.classList.remove("display");
+    
 });
