4b61416f2b3ef6923ab25d729e0ca48ee0b6a0d4
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-26 21:16:37 +0300

    added a new javascript file for the profile page,
    handle the sidenav,
    
    crop question or answer text if it was too large

---
+++ Rod/js/profile.js
@@ -0,0 +1,33 @@
+﻿let triggerSideNav = document.getElementById("triggerSideNav")
+let sidenav = document.getElementById("sidenav")
+let cropQuestionText = document.getElementById("ContentPlaceHolder1_QuestionsDataList_questionTitleText_0");
+let cropAnswerText = document.getElementById("ContentPlaceHolder1_AnswersDataList_answerTitleText_0");
+document.addEventListener('DOMContentLoaded', function () {
+
+    triggerSideNav.addEventListener("click", (event) => {
+        event.stopPropagation();
+        sidenav.classList.toggle("display")
+
+
+    })
+
+    document.addEventListener("click", function () {
+        sidenav.classList.remove("display");
+
+    });
+    function truncate(str, n) {
+        return str.length > n ? str.substr(0, n - 1) + '...' : str;
+    }
+
+    if (cropQuestionText != null) {
+
+        cropQuestionText.innerText = truncate(cropQuestionText.innerText, 20)
+    }
+    if (cropAnswerText != null) {
+
+        cropAnswerText.innerText = truncate(cropAnswerText.innerText, 20)
+    }
+    
+});
+
+
