724288405938b7d232ce02cf64988e0e1a8f7d40
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-26 21:15:27 +0300

    if the user open the search bar on small screens it will auto focus in the search textbox,
    added expectation in login page for profile menu

--- Rod/js/master.js
+++ Rod/js/master.js
@@ -5,13 +5,21 @@ searchInput.addEventListener("click", (event) => {
     event.stopPropagation();
     
     searchTextBox.classList.toggle("showTextBox")
+    window.setTimeout(() => searchTextBox.focus(), 100);
 })
+
+if (profileMenu != null) {
+
 profileMenu.addEventListener("click", (event) => {
     event.stopPropagation();
     dropdownContent.classList.toggle("drop")
     
 });
+}
 document.addEventListener("click", function () {
     searchTextBox.classList.remove("showTextBox");
+    if (profileMenu != null) {
+
     dropdownContent.classList.remove("drop")
+    }
 });
