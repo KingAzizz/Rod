5b797c1cb4d692466f06e511512408e66bfd4bdc
abdulaziz alsunaydi <azozb997@gmail.com>
2022-07-31 22:20:17 +0300

    added new javascript file for edit profile page

---
+++ Rod/js/editProfile.js
@@ -0,0 +1,29 @@
+﻿let uploadImageButton = document.getElementById("uploadImageLabel");
+let inputFile = document.getElementById("ContentPlaceHolder1_imageUpload")
+
+console.log("up and running");
+
+uploadImageButton.addEventListener("click", () => {
+    inputFile.click();
+    console.log(inputFile.type);
+})
+
+
+let triggerSideNav = document.getElementById("triggerSideNav")
+let sidenav = document.getElementById("ContentPlaceHolder1_sidenav")
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    triggerSideNav.addEventListener("click", (event) => {
+        event.stopPropagation();
+        sidenav.classList.toggle("display")
+
+
+    })
+});
+
+document.addEventListener("click", function () {
+    sidenav.classList.remove("display");
+
+});
