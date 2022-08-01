434eab9bc6567674122a3fff6d7efbb290ee9612
abdulaziz alsunaydi <azozb997@gmail.com>
2022-08-01 21:15:53 +0300

    remove unused code

--- Rod/js/profile.js
+++ Rod/js/profile.js
@@ -1,7 +1,5 @@
 ﻿let triggerSideNav = document.getElementById("triggerSideNav")
 let sidenav = document.getElementById("sidenav")
-let cropQuestionText = document.getElementById("ContentPlaceHolder1_QuestionsDataList_questionTitleText_0");
-let cropAnswerText = document.getElementById("ContentPlaceHolder1_AnswersDataList_answerTitleText_0");
 document.addEventListener('DOMContentLoaded', function () {
 
     triggerSideNav.addEventListener("click", (event) => {
@@ -15,19 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
         sidenav.classList.remove("display");
 
     });
-    function truncate(str, n) {
-        return str.length > n ? str.substr(0, n - 1) + '...' : str;
-    }
-
-    if (cropQuestionText != null) {
-
-        cropQuestionText.innerText = truncate(cropQuestionText.innerText, 20)
-    }
-    if (cropAnswerText != null) {
-
-        cropAnswerText.innerText = truncate(cropAnswerText.innerText, 20)
-    }
-    
 });
 
 
