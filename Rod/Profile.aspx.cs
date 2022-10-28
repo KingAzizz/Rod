f216338a0edbf79cf71edf74d3a63ad97ef1118e
Abdulaziz <azozb997@gmail.com>
2022-10-29 00:14:54 +0300

    db

--- Rod/Profile.aspx.cs
+++ Rod/Profile.aspx.cs
@@ -25,7 +25,7 @@ namespace Rod
     }
     public partial class Profile : System.Web.UI.Page
     {
-        public static string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Rodgit\Rod\App_Data\Rod.mdf;Integrated Security=True";
+        public static string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pc\Documents\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
 
         public string GetMonthDay(string dataValue)
         {
