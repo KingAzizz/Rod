307aa3a3b2db6e5738396f7f3781521b74036b8c
Abdulaziz <azozb997@gmail.com>
2022-10-29 00:17:38 +0300

    db

--- Rod/Registration.aspx.cs
+++ Rod/Registration.aspx.cs
@@ -28,7 +28,7 @@ namespace Rod
         protected void Reg(object sender, EventArgs e)
         {
            
-            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Rodgit\Rod\App_Data\Rod.mdf;Integrated Security=True";
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pc\Documents\Rod\Rod\App_Data\Rod.mdf;Integrated Security=True";
             using (SqlConnection connection = new SqlConnection(cs))
             {
                 try
@@ -53,7 +53,7 @@ namespace Rod
 
               catch (Exception ee)
                 {
-                    MessageBox.Show("المستخدم موجود مسبقاً");
+                    badReg.Visible = true;
                 }
                    
             }
